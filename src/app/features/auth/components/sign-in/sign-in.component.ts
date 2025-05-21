b7fbe22d7eb9d8f27cfca740a8d0c6704d9bc06c
Djonathan D'Avila <123762143+DjonathanDAvila@users.noreply.github.com>
2025-05-21 14:05:09 -0300

    feat: Adicionado "loadin" ao aguardar login git

--- src/app/features/auth/components/sign-in/sign-in.component.ts
+++ src/app/features/auth/components/sign-in/sign-in.component.ts
@@ -3,16 +3,20 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule, RouterModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, RouterModule,MatProgressSpinnerModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
 export class SignInComponent {
+  isLoading = false;
+
   loginWithGithub() {
+    this.isLoading = true;
     window.location.href = 'http://localhost:5021/api/v1/github/login';
   }
 }
